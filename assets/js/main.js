7572bd01798d6f833067760170ad2cc814246ec4
Anupam Khosla <anupamkhosla1993@gmail.com>
2021-12-09 00:40:02 +0530

    Working on homepage day2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,64 @@
 "use strict";
-$(document).ready(function() {
-    
-    $("select").selectric();
-    $(".slick").slick({});
+$(document).ready(function(){
+  
+  $("select").selectric();
+  $(".slick").slick({
+      // normal options...
+      infinite: false,
+      slidesToShow: 5,
+      // the magic
+      responsive: [        
+      {
+        breakpoint: 1400,
+        settings: {
+          slidesToShow: 4            
+        }
+      }, 
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+        }
+      }, 
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+        }
+      }
+      ]
+    });
 
 
+  /*toggle filters functionality on phone*/
+  !function(){
 
 
+    if($(".sort.go").is(":visible")) {
+     $(".sort-dropdown").hide()
+     $(".sort-dropdown").removeClass("d-none d-md-flex");
 
+     $(".sort").click(function(){
+      if($(this).hasClass("active")){
+        $(this).removeClass("active");
+        $(".sort-dropdown").slideUp();
+      }
+      else {
+        $(this).addClass("active");
+        $(".sort-dropdown").slideDown();
+      }
+    });
+   }
 
+   
 
+ }();
 
+ /*end*/
 });
